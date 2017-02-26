fc93563aa7de0b4d2bf15ee5133fb187cec54297
Sam Ober <ober.sam@gmail.com>
2017-02-26 08:44:22 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from Cython.Build import cythonize
 import numpy
 
 ext_modules = []
-ext_modules.append(Extension("ober.db.tokens.tokens_inner", ["ober/db/tokens/tokens_inner.pyx"], include_dirs=[numpy.get_include()]))
+ext_modules.append(Extension("ober.tokens.tokens_inner", ["ober/tokens/tokens_inner.pyx"], include_dirs=[numpy.get_include()]))
 
 setup(
-    name="ober",
-    packages=["ober", 
-              "ober.db", 
-              "ober.db.documents", 
-              "ober.db.tokens", 
+    name="ober-ml",
+    packages=["ober",
+              "ober.documents", 
+              "ober.tokens", 
+              "ober.senses",
               "ober.models", 
               "ober.models.phrases",
               "ober.models.sense2vec",
